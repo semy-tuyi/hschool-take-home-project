3b7d13b153ddec59a90c39dddf8984eade42f810
semy-tuyi <tuyisengesamy6@.gmail.com>
2023-10-22 17:19:30 +0200

    setting up course state

--- src/app/components/molecules/course-name/course-name.component.ts
+++ src/app/components/molecules/course-name/course-name.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CourseInterface } from 'src/app/store/Model/Course';
 
 @Component({
   selector: 'app-course-name',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./course-name.component.css']
 })
 export class CourseNameComponent {
+  @Input () course:CourseInterface={
+    id:0,
+    name:'',
+    author:'',
+    progress:1,
+    lessons:1
+  };
+
+  constructor(){console.warn(this.course.name)}
+
 
 }
